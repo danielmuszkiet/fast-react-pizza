e7352304c5c10046fa3ae5f2ec6eb524cab81e86
Daniel Muszkiet <d.muszkiet@elo.com>
2025-07-08 15:05:13 +0200

    feat: add pizza SVG icon, enhance cart and order components with improved styling and structure

--- src/features/cart/CartItem.tsx
+++ src/features/cart/CartItem.tsx
@@ -1,18 +1,20 @@
 import { formatCurrency } from "../../utils/helpers";
 import type { TItem } from "../../types";
+import Button from "../../ui/Button";
 
-function CartItem(item: TItem) {
+function CartItem({ item }: { item: TItem }) {
   //pizzaId
 
   const { name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -6,17 +6,19 @@ function Button({
   disabled,
   children,
 }: {
-  type?: "small" | "primary";
+  type?: "small" | "primary" | "secondary";
   to?: string;
   disabled?: boolean;
   children: React.ReactNode;
 }) {
   const base =
-    "inline-block cursor-pointer rounded-full bg-yellow-400  font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed" +
+    "text-sm inline-block cursor-pointer rounded-full bg-yellow-400  font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed" +
     " ";
 
   const styles = {
-    primary: base + "px-4 py-3 md:px-6 md:py-4",
+    primary: base + "px-4 py-2 md:px-6 md:py-3 sm:text-base",
+    secondary:
+      "text-sm inline-block cursor-pointer rounded-full font-semibold border-2 border-stone-300 tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed px-4 py-1.5 md:px-6 md:py-2.5 sm:text-base",
     small: base + "px-4 py-2 md:px-5 md:py-2 text-xs",
   };
 
