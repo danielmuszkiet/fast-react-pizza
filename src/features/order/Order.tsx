3407a2ebfb1fc9a6a97e7d603e9c5bfacad0e239
Daniel Muszkiet <d.muszkiet@elo.com>
2025-07-09 14:25:07 +0200

    feat: enhance Order and OrderItem components with improved layout and styling

--- src/features/order/Order.tsx
+++ src/features/order/Order.tsx
@@ -1,77 +1,78 @@
 // Test ID: IIDSAT or CQE92U
 
-import { calcMinutesLeft, formatCurrency, formatDate } from "../../utils/helpers";
+import {
+  calcMinutesLeft,
+  formatCurrency,
+  formatDate,
+} from "../../utils/helpers";
 
 import type { Order as TOrder } from "../../types";
 import { useLoaderData } from "react-router";
-
-// const order: TOrder = {
-//   id: "ABCDEF",
-//   customer: "Jonas",
-//   phone: "123456789",
-//   address: "Arroios, Lisbon , Portugal",
-//   priority: true,
-//   estimatedDelivery: "2027-04-25T10:00:00",
-//   cart: [
-//     {
-//       pizzaId: 7,
-//       name: "Napoli",
-//       quantity: 3,
-//       unitPrice: 16,
-//       totalPrice: 48,
-//     },
-//     {
-//       pizzaId: 5,
-//       name: "Diavola",
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//     },
-//     {
-//       pizzaId: 3,
-//       name: "Romana",
-//       quantity: 1,
-//       unitPrice: 15,
-//       totalPrice: 15,
-//     },
-//   ],
-//   position: "-9.000,38.000",
-//   orderPrice: 95,
-//   priorityPrice: 19,
-// };
+import OrderItem from "./OrderItem";
 
 function Order() {
   const order = useLoaderData<TOrder>();
 
-  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+  // Everyone can search for all orders, so for privacy reasons we're gonna gonna
+  // exclude names or address, these are only for the restaurant staff
   // id, status, cart
-  const { id, status, cart, priority, priorityPrice, orderPrice, estimatedDelivery } = order;
+  const {
+    id,
+    status,
+    cart,
+    priority,
+    priorityPrice,
+    orderPrice,
+    estimatedDelivery,
+  } = order;
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div>
-      <div>
-        <h2>Status</h2>
+    <div className="space-y-8 px-4 py-6">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-xl font-semibold">Order #{id} status</h2>
 
-        <div>
-          {priority && <span>Priority</span>}
-          <span>{status} order</span>
+        <div className="space-x-2">
+          {priority && (
+            <span className="rounded-full bg-red-400 px-3 py-1 text-sm font-semibold tracking-wide text-red-50 uppercase">
+              Priority
+            </span>
+          )}
+          <span className="rounded-full bg-green-400 px-3 py-1 text-sm font-semibold tracking-wide text-green-50 uppercase">
+            {status} order
+          </span>
         </div>
       </div>
 
-      <div>
-        <p>
+      <div className="flex flex-wrap items-center justify-between gap-2 rounded-sm bg-stone-200 px-6 py-5">
+        <p className="font-medium">
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
             : "Order should have arrived"}
         </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p className="text-xs text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
       </div>
 
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      <ul className="divide-y divide-stone-200 border-y border-stone-300">
+        {cart.map((item) => {
+          return <OrderItem item={item} key={item.pizzaId} />;
+        })}
+      </ul>
+
+      <div className="space-y-2 rounded-sm bg-stone-200 px-6 py-5">
+        <p className="text-sm font-medium text-stone-600">
+          Price pizza: {formatCurrency(orderPrice)}
+        </p>
+        {priority && (
+          <p className="text-sm font-medium text-stone-600">
+            Price priority: {formatCurrency(priorityPrice)}
+          </p>
+        )}
+        <p className="font-bold">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   );
